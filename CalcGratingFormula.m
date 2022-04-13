41ea8c1379fc732fe6b0621b8a4d1e2175debe10
Jerry Yan <jerry0317@users.noreply.github.com>
2022-04-13 14:14:13 -0700

    Create CalcGratingFormula.m

---
+++ CalcGratingFormula.m
@@ -0,0 +1,20 @@
+function [m, Lambda, d, Theta] = CalcGratingFormula(m, Lambda, d, Theta)
+    % d*sin(Theta) = m * Lambda
+    
+    if m == 0
+        m = d * sin(Theta) / Lambda;
+    end
+    
+    if Lambda == 0
+        Lambda = d * sin(Theta) / m;
+    end
+
+    if d == 0
+        d = m * Lambda / sin(Theta);
+    end
+
+    if Theta == 0
+        Theta = asin(m * Lambda / d);
+    end
+    
+end
